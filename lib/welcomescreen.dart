d017aea6f27d6f28cc1b88882c34577088c66fac
Aditya Phadatare <adityaphadatare4545@gmail.com>
2025-06-24 18:36:19 +0530

    Initial Code

---
+++ lib/welcomescreen.dart
@@ -0,0 +1,61 @@
+// To-Do list 
+/*
+1. Create appbar 
+2. Design the layout
+*/
+import 'package:flutter/material.dart';
+
+class Welcomescreen extends StatelessWidget{
+  const Welcomescreen({super.key});
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+     appBar: AppBar(
+       backgroundColor: Color.fromRGBO(51, 49, 47, 1),
+       elevation: 0,
+       title: const Text(
+        "DevTools",
+       style: TextStyle(
+        color: Colors.white,
+        fontWeight: FontWeight.bold,        
+        ),       
+       ),
+       centerTitle: true,
+       leading: Builder(
+        builder:(context){
+          return IconButton( 
+            icon: const Icon(Icons.menu),
+            color: Colors.white,
+          onPressed: (){
+            Scaffold.of(context).openDrawer();
+          }
+          );
+        }
+      ),
+     ),
+     body: Padding(
+       padding: const EdgeInsets.all(24),
+       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          const SizedBox(height: 10),
+          const Align(
+            alignment: Alignment.centerLeft,
+              child: Text(
+                'Welcome \n to\nDevTools',
+                style: TextStyle(
+                  fontSize: 45,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+          ),
+          const SizedBox(height: 20,),
+         // ElevatedButton(onPressed: onPressed, child: child)
+        ],
+       ),
+     ),
+    );
+  }
+  
+}
